51cdb1e82b3df2624d0d191adbb7eff6123a25a4
Himangshu <hdas9599@gmail.com>
2025-10-29 08:37:43 +0530

    trying to fix img src

--- script/randomImage.js
+++ script/randomImage.js
@@ -1,7 +1,7 @@
-// const images = ["../assets/bg/bg1.jpg", "../assets/bg/bg2.jpg", "../assets/bg/bg3.jpg"]
-// const randomNum = Math.floor(Math.random()*images.length);
-// document.write(
-//   `<style>#background{
-//   background: url("${images[randomNum]}") no-repeat center / cover !important;
-//   } </style>`
-// )
+const images = ["../assets/bg/bg1.jpg", "../assets/bg/bg2.jpg", "../assets/bg/bg3.jpg"]
+const randomNum = Math.floor(Math.random()*images.length);
+document.write(
+  `<style>#background{
+  background: url("${images[randomNum]}") no-repeat center / cover !important;
+  } </style>`
+)
