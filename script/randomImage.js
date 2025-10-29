336d1c0d0da8f6da58f819c7325e48e3906ecbef
Himangshu <hdas9599@gmail.com>
2025-10-29 13:19:18 +0530

    adding random bg feature

--- script/randomImage.js
+++ script/randomImage.js
@@ -1,7 +1,7 @@
-// const images = ["../assets/bg/bg1.jpg", "../assets/bg/bg2.jpg", "../assets/bg/bg3.jpg"]
-// const randomNum = Math.floor(Math.random()*images.length);
-// document.write(
-//   `<style>#background{
-//   background: url("${images[randomNum]}") no-repeat center / cover !important;
-//   } </style>`
-// )
+const images = ["bg1.jpg", "bg2.jpg", "bg3.jpg"]
+const randomNum = Math.floor(Math.random()*images.length);
+document.write(
+  `<style>#background{
+  background: url("./assets/image/${images[randomNum]}") no-repeat center / cover !important;
+  } </style>`
+)
